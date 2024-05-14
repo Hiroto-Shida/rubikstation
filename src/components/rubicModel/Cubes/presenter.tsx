fb558ec8184c090f92c60ea2eac8f5a53c76724a
Hiroto-Shida <uecshida@gmail.com>
2024-05-15 01:24:35 +0900

    fix: スクランブル画像を3Dモデルで表示

--- src/components/rubicModel/Cubes/presenter.tsx
+++ src/components/rubicModel/Cubes/presenter.tsx
@@ -1,17 +1,17 @@
-import * as THREE from "three";
-import { SURFACE_COLORS } from "../surfaceColors";
-import { Axis, Limit, Multiplier, ROTATE_DIRECTION } from "../rotateDirection";
-import { useEffect, useRef } from "react";
+import * as THREE from "three"
+import { SURFACE_COLORS } from "../surfaceColors"
+import { Axis, Limit, Multiplier, ROTATE_DIRECTION } from "../rotateDirection"
+import { useEffect, useRef } from "react"
 
 const resetCubeGroup = (cubeGroup: THREE.Group, rotationGroup: THREE.Group) => {
   rotationGroup.children
     .slice()
     .reverse()
     .forEach(function (c) {
-      cubeGroup.attach(c);
-    });
-  rotationGroup.quaternion.set(0, 0, 0, 1);
-};
+      cubeGroup.attach(c)
+    })
+  rotationGroup.quaternion.set(0, 0, 0, 1)
+}
 
 const attachToRotationGroup = (
   cubeGroup: THREE.Group,
@@ -23,20 +23,20 @@ const attachToRotationGroup = (
     .slice()
     .reverse()
     .filter(function (c) {
-      return limit < 0 ? c.position[axis] < limit : c.position[axis] > limit;
+      return limit < 0 ? c.position[axis] < limit : c.position[axis] > limit
     })
     .forEach(function (c) {
-      rotationGroup.attach(c);
-    });
-};
+      rotationGroup.attach(c)
+    })
+}
 
 const rotateGroup = (
   rotationGroup: THREE.Group,
   axis: Axis,
   multiplier: Multiplier
 ) => {
-  rotationGroup.rotation[axis] += (Math.PI / 6) * multiplier;
-};
+  rotationGroup.rotation[axis] += (Math.PI / 6) * multiplier
+}
 
 const addArrow = (
   rotationGroup: THREE.Group,
@@ -44,100 +44,110 @@ const addArrow = (
   limit: Limit,
   multiplier: Multiplier
 ) => {
-  let countIndex = 0;
+  let countIndex = 0
   rotationGroup.children.forEach((child) => {
     if (child instanceof THREE.Mesh) {
-      const coneGeometry = new THREE.ConeGeometry(0.2, 1, 10);
-      const cylinderGeometry = new THREE.CylinderGeometry(0.1, 0.1, 2, 10);
-      const material = new THREE.MeshBasicMaterial({ color: "#272222" });
+      const coneGeometry = new THREE.ConeGeometry(0.2, 1, 10)
+      const cylinderGeometry = new THREE.CylinderGeometry(0.1, 0.1, 2, 10)
 
-      const arrowGroup = new THREE.Group();
-      const coneMesh = new THREE.Mesh(coneGeometry, material);
-      coneMesh.position.setY(1);
-      const cylinderMesh = new THREE.Mesh(cylinderGeometry, material);
-      arrowGroup.add(coneMesh);
-      arrowGroup.add(cylinderMesh);
+      const meshMaterial = new THREE.MeshStandardMaterial({
+        color: "#ffffff",
+        emissive: "#ffffff",
+      })
+      const lineMaterial = new THREE.MeshStandardMaterial({
+        color: "#ff0000",
+        emissive: "#ff0000",
+      })
 
-      const distanceFromSurface = 0.7;
+      const arrowGroup = new THREE.Group()
 
-      const childPos = child.position.toArray();
-      console.log(childPos);
+      const coneMesh = new THREE.Mesh(coneGeometry, meshMaterial)
 
-      const originAxis = ["x", "y", "z"];
+      const coneWireframe = new THREE.WireframeGeometry(coneGeometry)
+      const coneLine = new THREE.LineSegments(coneWireframe, lineMaterial)
+
+      coneMesh.position.setY(1)
+      coneLine.position.setY(1)
+
+      const cylinderMesh = new THREE.Mesh(cylinderGeometry, meshMaterial)
+
+      arrowGroup.add(coneMesh)
+      arrowGroup.add(coneLine)
+      arrowGroup.add(cylinderMesh)
+
+      const distanceFromSurface = 0.7
+
+      const childPos = child.position.toArray()
+
+      const originAxis = ["x", "y", "z"]
 
       const otherAxis = originAxis.filter((axis) => axis !== rotateAxis) as (
         | "x"
         | "y"
         | "z"
-      )[];
+      )[]
 
       // 矢印をつける対象のCubeの場合、isTargetAddArrow = true
-      let isTargetAddArrow;
+      let isTargetAddArrow
       if (Math.sign(limit) == 1) {
         isTargetAddArrow =
           childPos.filter((pos) => pos === 1).length == 2 &&
-          childPos.filter((pos) => pos === 0).length == 1;
+          childPos.filter((pos) => pos === 0).length == 1
       } else {
         isTargetAddArrow =
           childPos.filter((pos) => pos === 1).length == 1 &&
           childPos.filter((pos) => pos === -1).length == 1 &&
-          childPos.filter((pos) => pos === 0).length == 1;
+          childPos.filter((pos) => pos === 0).length == 1
       }
 
       if (isTargetAddArrow) {
         if (otherAxis[countIndex] === "x") {
-          arrowGroup.position.setX(distanceFromSurface);
-          // console.log("pos x");
+          arrowGroup.position.setX(distanceFromSurface)
         }
         if (otherAxis[countIndex] === "y") {
-          arrowGroup.position.setY(distanceFromSurface);
-          // console.log("pos y");
+          arrowGroup.position.setY(distanceFromSurface)
         }
         if (otherAxis[countIndex] === "z") {
-          arrowGroup.position.setZ(distanceFromSurface);
-          // console.log("pos z");
+          arrowGroup.position.setZ(distanceFromSurface)
         }
 
         if (rotateAxis === "z") {
           if (multiplier === -1) {
-            arrowGroup.rotation.x = Math.PI;
+            arrowGroup.rotation.x = Math.PI
           }
           if (originAxis[childPos.indexOf(0)] == "x") {
-            arrowGroup.rotation.z += (multiplier * Math.PI) / 2;
+            arrowGroup.rotation.z += (multiplier * Math.PI) / 2
           }
         }
 
         if (rotateAxis === "x") {
           if (multiplier === 1) {
-            arrowGroup.rotation.x = Math.PI;
+            arrowGroup.rotation.x = Math.PI
           }
           if (originAxis[childPos.indexOf(0)] == "z") {
-            arrowGroup.rotation.x += Math.PI / 2;
-            console.log("rotate x");
+            arrowGroup.rotation.x += Math.PI / 2
           }
         }
 
         if (rotateAxis === "y") {
           if (multiplier === 1) {
-            arrowGroup.rotation.x = Math.PI;
+            arrowGroup.rotation.x = Math.PI
           }
           if (originAxis[childPos.indexOf(0)] == "x") {
-            arrowGroup.rotation.z -= (multiplier * Math.PI) / 2;
-            console.log("rotate z");
+            arrowGroup.rotation.z -= (multiplier * Math.PI) / 2
           }
           if (originAxis[childPos.indexOf(0)] == "z") {
-            arrowGroup.rotation.x += Math.PI / 2;
-            console.log("rotate x");
+            arrowGroup.rotation.x += Math.PI / 2
           }
         }
 
-        arrowGroup.scale.set(0.9, 0.9, 0.9);
-        child.add(arrowGroup);
-        countIndex += 1;
+        arrowGroup.scale.set(0.9, 0.9, 0.9)
+        child.add(arrowGroup)
+        countIndex += 1
       }
     }
-  });
-};
+  })
+}
 
 const rotate = (
   cubeGroup: THREE.Group,
@@ -146,18 +156,18 @@ const rotate = (
   limit: Limit,
   multiplier: Multiplier
 ) => {
-  resetCubeGroup(cubeGroup, rotationGroup);
-  attachToRotationGroup(cubeGroup, rotationGroup, axis, limit);
-  rotateGroup(rotationGroup, axis, multiplier);
-  addArrow(rotationGroup, axis, limit, multiplier);
-};
+  resetCubeGroup(cubeGroup, rotationGroup)
+  attachToRotationGroup(cubeGroup, rotationGroup, axis, limit)
+  rotateGroup(rotationGroup, axis, multiplier)
+  addArrow(rotationGroup, axis, limit, multiplier)
+}
 
 const Cube = ({
   position,
   colorList,
 }: {
-  position: [number, number, number];
-  colorList: string[];
+  position: [number, number, number]
+  colorList: string[]
 }) => {
   const colorsDic: { [key: string]: string } = {
     green: "#188a28",
@@ -166,7 +176,7 @@ const Cube = ({
     white: "#ffffff",
     orange: "#ff8005",
     blue: "#004ac3",
-  }; // 6 colors for the faces
+  } // 6 colors for the faces
 
   return (
     <mesh position={position}>
@@ -179,28 +189,30 @@ const Cube = ({
         />
       ))}
     </mesh>
-  );
-};
+  )
+}
 
 export const CubesPresenter = ({ moveChar }: { moveChar: string }) => {
-  const cubeGroup = useRef<THREE.Group>(null!);
-  const rotationGroup = useRef<THREE.Group>(null!);
+  const cubeGroup = useRef<THREE.Group>(null!)
+  const rotationGroup = useRef<THREE.Group>(null!)
 
   useEffect(() => {
     if (cubeGroup.current && rotationGroup.current) {
-      if (ROTATE_DIRECTION[moveChar]) {
+      const regexMoveChar = /2/
+      const removedTwoMoveChar = moveChar.replace(regexMoveChar, "")
+      if (ROTATE_DIRECTION[removedTwoMoveChar]) {
         rotate(
           cubeGroup.current,
           rotationGroup.current,
-          ROTATE_DIRECTION[moveChar][0],
-          ROTATE_DIRECTION[moveChar][1],
-          ROTATE_DIRECTION[moveChar][2]
-        );
+          ROTATE_DIRECTION[removedTwoMoveChar][0],
+          ROTATE_DIRECTION[removedTwoMoveChar][1],
+          ROTATE_DIRECTION[removedTwoMoveChar][2]
+        )
       } else {
-        console.log(`回転記号 ${moveChar} は存在しません`);
+        console.log(`回転記号 ${moveChar} は存在しません`)
       }
     }
-  }, []);
+  }, [])
 
   return (
     <>
@@ -219,5 +231,5 @@ export const CubesPresenter = ({ moveChar }: { moveChar: string }) => {
       </group>
       <group ref={rotationGroup}></group>
     </>
-  );
-};
+  )
+}
--- src/components/scrambleText/ScrambleText/presenter.tsx
+++ src/components/scrambleText/ScrambleText/presenter.tsx
@@ -1,6 +1,7 @@
-import { ImageList, ImageListItem, Typography } from "@mui/material";
-import { useContext, useEffect, useState } from "react";
-import { RunningStateContext } from "../../../pages/Index/presenter";
+import { Grid, ImageList, ImageListItem, Typography } from "@mui/material"
+import { useContext, useEffect, useState } from "react"
+import { RunningStateContext } from "../../../pages/Index/presenter"
+import { RubicModel } from "../../rubicModel/RubicModel/container"
 
 // 記号のリストと、「'そのまま'、'2回転'、'逆回転'」のオプションリスト
 const MOVE_LIST: string[] = ["U", "F", "R", "D", "B", "L"]
@@ -15,7 +16,7 @@ const ROW_MAX_LEGTH = 7
 
 // min 以上 max 以下 の範囲のランダム整数を取得
 const getRandomArbitrary = (min: number, max: number): number => {
-  return Math.floor(Math.random() * (max + 1 - min) + min);
+  return Math.floor(Math.random() * (max + 1 - min) + min)
 }
 
 // Listからランダムに値を取得
@@ -25,23 +26,24 @@ const getRandomValueFromList = (targetList: string[]): string => {
 
 // スクランブルの記号リスト(ROW_MAX_LEGTH区切りの2次元配列)
 const generateScrambleText = (): string[][] => {
-  const scrambleSize = getRandomArbitrary(MIN_MOVE,MAX_MOVE)
+  const scrambleSize = getRandomArbitrary(MIN_MOVE, MAX_MOVE)
   const textList: string[] = []
 
   let originChar = "" // ランダムに取り出した値かつ、直前値として格納
   for (let index = 0; index < scrambleSize; index++) {
-    originChar = getRandomValueFromList(MOVE_LIST.filter(value => value !== originChar)) // 直前の記号以外からランダムに取得
+    originChar = getRandomValueFromList(
+      MOVE_LIST.filter((value) => value !== originChar)
+    ) // 直前の記号以外からランダムに取得
     const optionChar = getRandomValueFromList(OPTION_MOVE_LIST)
-    textList.push(originChar+optionChar)
+    textList.push(originChar + optionChar)
   }
 
   const tmpMultiTextList: string[][] = []
-  for (let index = 0; index < textList.length; index=index+ROW_MAX_LEGTH) {
-    tmpMultiTextList.push(textList.slice(index, index+ROW_MAX_LEGTH))
+  for (let index = 0; index < textList.length; index = index + ROW_MAX_LEGTH) {
+    tmpMultiTextList.push(textList.slice(index, index + ROW_MAX_LEGTH))
   }
   return tmpMultiTextList
 }
-
 
 export const ScrambleTextPresenter = () => {
   const [multiTextList, setMultiTextList] = useState<string[][]>([[]])
@@ -50,26 +52,22 @@ export const ScrambleTextPresenter = () => {
 
   useEffect(() => {
     if (runningState.isStay) {
-      setMultiTextList(generateScrambleText());
-    }else{
-      setMultiTextList([[]]);
+      setMultiTextList(generateScrambleText())
+    } else {
+      setMultiTextList([[]])
     }
-  }, [runningState]);
+  }, [runningState])
 
   if (runningState.isStarted) {
     return (
       <>
-        <Typography variant="h4">
-          Press Space
-        </Typography>
+        <Typography variant="h4">Press Space</Typography>
       </>
     )
   }
 
   if (runningState.isPause) {
-    return (
-      <></>
-    )
+    return <></>
   }
 
   if (runningState.isStay) {
@@ -77,36 +75,23 @@ export const ScrambleTextPresenter = () => {
       <>
         {multiTextList[0].length ? (
           <>
-          {multiTextList.map((childList, index) => (
-            <Typography variant="h4" key={index}>
-              {childList.join(' ')}
-            </Typography>
-          ))}
-          <ImageList cols={ROW_MAX_LEGTH} rowHeight={100}>
-            {multiTextList.map((childList) => (
-              childList.map((moveName, index) => (
-                <ImageListItem key={index}>
-                  <img
-                    style={{
-                      width: '100px',
-                      height: '100px'
-                    }}
-                    srcSet={`/move_images/${moveName}.png 268w`}
-                    src={`/move_images/${moveName}.png`}
-                    alt={moveName}
-                    loading="lazy"
-                  />
-                </ImageListItem>
-              ))
+            {multiTextList.map((childList, index) => (
+              <Typography variant="h4" key={index}>
+                {childList.join(" ")}
+              </Typography>
             ))}
-          </ImageList>
-        </>
+            <Grid container>
+              {multiTextList.map((childList) =>
+                childList.map((moveName, index) => (
+                  <RubicModel key={index} moveChar={moveName} />
+                ))
+              )}
+            </Grid>
+          </>
         ) : (
-          <Typography variant="h4">
-            Press Space
-          </Typography>
+          <Typography variant="h4">Press Space</Typography>
         )}
       </>
     )
   }
-};
+}
