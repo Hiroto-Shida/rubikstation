626588c654e98e45601ab6b85a66ac4d595fdb6f
Hiroto-Shida <uecshida@gmail.com>
2024-05-15 22:49:28 +0900

    fix: package.jsonで行なっていたdocsフォルダへのコピーをvite.config.tsで行なうように設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig, loadEnv } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
 // export default defineConfig({
 //   base: process.env.GITHUB_PAGES
@@ -9,10 +9,11 @@ import react from "@vitejs/plugin-react-swc";
 // });
 
 export default defineConfig(({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
   return defineConfig({
+    build: { outDir: "docs" },
     base: process.env.VITE_BASE_URL,
     plugins: [react()],
-  });
-});
+  })
+})
