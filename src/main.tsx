dd5e6eb92c479e7bc5eb34dde4b0854dcdd86385
Hiroto-Shida <uecshida@gmail.com>
2024-05-16 00:14:29 +0900

    fix: docsの作り直し。無駄なurlパスを削除

--- src/main.tsx
+++ src/main.tsx
@@ -22,7 +22,6 @@ const theme = createTheme({
   },
 })
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react-swc"
 
-// export default defineConfig({
-//   base: process.env.GITHUB_PAGES
-//     ? "./rubic-timer/"
-//     : "./",
-//   plugins: [react()],
-// });
-
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
