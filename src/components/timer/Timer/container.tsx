4b77e4d9d2e90834ce2ed551c05b67eb2bc2760f
Hiroto-Shida <uecshida@gmail.com>
2024-05-17 00:03:17 +0900

    fix: timerのcontainer, presenter整理

--- src/components/timer/Timer/container.tsx
+++ src/components/timer/Timer/container.tsx
@@ -1,5 +1,39 @@
-import { TimerPresenter } from "./presenter";
+import { useContext, useEffect, useRef, useState } from "react"
+import { TimerPresenter } from "./presenter"
+import { TimerStateContext } from "../../../providers/TimerStateProvider"
 
 export const Timer = () => {
-  return <TimerPresenter />;
-};
+  const [time, setTime] = useState<number>(0)
+  const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null)
+
+  const timerState = useContext(TimerStateContext)
+
+  function handleStart() {
+    intervalRef.current = setInterval(() => {
+      setTime((prevTime) => prevTime + 10)
+    }, 10)
+  }
+
+  function handlePause() {
+    intervalRef.current && clearInterval(intervalRef.current)
+  }
+
+  function handleReset() {
+    intervalRef.current && clearInterval(intervalRef.current)
+    setTime(0)
+  }
+
+  useEffect(() => {
+    if (timerState.isStarted) {
+      handleStart()
+    }
+    if (timerState.isPause) {
+      handlePause()
+    }
+    if (timerState.isStay) {
+      handleReset()
+    }
+  }, [timerState])
+
+  return <TimerPresenter time={time} />
+}
--- src/components/timer/Timer/presenter.tsx
+++ src/components/timer/Timer/presenter.tsx
@@ -1,40 +1,10 @@
 import { Typography } from "@mui/material"
-import { useState, useRef, useEffect, useContext } from "react"
-import { TimerStateContext } from "../../../providers/TimerStateProvider"
 
-export const TimerPresenter = () => {
-  const [time, setTime] = useState<number>(0)
-  const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null)
+type Props = {
+  time: number
+}
 
-  const timerState = useContext(TimerStateContext)
-
-  function handleStart() {
-    intervalRef.current = setInterval(() => {
-      setTime((prevTime) => prevTime + 10)
-    }, 10)
-  }
-
-  function handlePause() {
-    intervalRef.current && clearInterval(intervalRef.current)
-  }
-
-  function handleReset() {
-    intervalRef.current && clearInterval(intervalRef.current)
-    setTime(0)
-  }
-
-  useEffect(() => {
-    if (timerState.isStarted) {
-      handleStart()
-    }
-    if (timerState.isPause) {
-      handlePause()
-    }
-    if (timerState.isStay) {
-      handleReset()
-    }
-  }, [timerState])
-
+export const TimerPresenter = ({ time }: Props) => {
   const milliseconds = `0${(time % 1000) / 10}`.slice(-2)
   const seconds = `0${Math.floor(time / 1000) % 60}`.slice(-2)
   const minutes = `0${Math.floor(time / 60000) % 60}`.slice(-2)
@@ -42,7 +12,7 @@ export const TimerPresenter = () => {
   return (
     <>
       <Typography variant="h4">
-        {minutes}:{seconds}:{milliseconds}
+        {minutes}:{seconds}.{milliseconds}
       </Typography>
     </>
   )
