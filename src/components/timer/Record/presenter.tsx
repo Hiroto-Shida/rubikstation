22f0f29055be69abee661f733c6af40cbc606e31
Hiroto-Shida <uecshida6295@gmail.com>
2024-05-30 11:17:02 +0900

    fix: READMEのv1.2.0に追記 fix: AO12の記録表示の「O」が抜けていたのを修正

--- src/components/timer/Record/presenter.tsx
+++ src/components/timer/Record/presenter.tsx
@@ -8,29 +8,41 @@ type Props = {
   ao12: number;
 };
 
-export const RecordPresenter = React.memo(({ timerState, ao5, ao12 }: Props) => {
-  const isDisplay: boolean = !timerState.isStarted && !timerState.startingState.isCanStart;
+export const RecordPresenter = React.memo(
+  ({ timerState, ao5, ao12 }: Props) => {
+    const isDisplay: boolean =
+      !timerState.isStarted && !timerState.startingState.isCanStart;
 
-  const ao5_milliseconds = `0${Math.round((ao5 % 1000) / 10)}`.slice(-2);
-  const ao5_seconds = `0${Math.floor(ao5 / 1000) % 60}`.slice(-2);
-  const ao5_minutes = `0${Math.floor(ao5 / 60000) % 60}`.slice(-2);
+    const ao5_milliseconds = `0${Math.round((ao5 % 1000) / 10)}`.slice(-2);
+    const ao5_seconds = `0${Math.floor(ao5 / 1000) % 60}`.slice(-2);
+    const ao5_minutes = `0${Math.floor(ao5 / 60000) % 60}`.slice(-2);
 
-  const ao12_milliseconds = `0${Math.round((ao12 % 1000) / 10)}`.slice(-2);
-  const ao12_seconds = `0${Math.floor(ao12 / 1000) % 60}`.slice(-2);
-  const ao12_minutes = `0${Math.floor(ao12 / 60000) % 60}`.slice(-2);
+    const ao12_milliseconds = `0${Math.round((ao12 % 1000) / 10)}`.slice(-2);
+    const ao12_seconds = `0${Math.floor(ao12 / 1000) % 60}`.slice(-2);
+    const ao12_minutes = `0${Math.floor(ao12 / 60000) % 60}`.slice(-2);
 
-  return (
-    isDisplay && (
-      <>
-        <Box component="div" sx={{ display: "flex", justifyContent: "center" }}>
-          <Typography variant="h6">
-            AO5: {ao5 === 0 ? "-" : `(${ao5_minutes}:${ao5_seconds}:${ao5_milliseconds})`}
-          </Typography>
-          <Typography variant="h6" sx={(theme) => ({ ml: theme.spacing(3) })}>
-            A12: {ao12 === 0 ? "-" : `(${ao12_minutes}:${ao12_seconds}:${ao12_milliseconds})`}
-          </Typography>
-        </Box>
-      </>
-    )
-  );
-});
+    return (
+      isDisplay && (
+        <>
+          <Box
+            component="div"
+            sx={{ display: "flex", justifyContent: "center" }}
+          >
+            <Typography variant="h6">
+              AO5:{" "}
+              {ao5 === 0
+                ? "-"
+                : `(${ao5_minutes}:${ao5_seconds}:${ao5_milliseconds})`}
+            </Typography>
+            <Typography variant="h6" sx={(theme) => ({ ml: theme.spacing(3) })}>
+              AO12:{" "}
+              {ao12 === 0
+                ? "-"
+                : `(${ao12_minutes}:${ao12_seconds}:${ao12_milliseconds})`}
+            </Typography>
+          </Box>
+        </>
+      )
+    );
+  }
+);
