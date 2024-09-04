1e80324bb46af5c9e0abea7cb794938b3c04e1aa
Hiroto-Shida <uecshida6295@gmail.com>
2024-09-04 22:47:38 +0900

    リリース情報とREADME追記

--- src/pages/Release/presenter.tsx
+++ src/pages/Release/presenter.tsx
@@ -1,4 +1,12 @@
-import { Typography, Divider, List, ListItem, ListItemText, Box, Theme } from "@mui/material";
+import {
+  Typography,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  Box,
+  Theme,
+} from "@mui/material";
 import { ReactNode } from "react";
 
 const StyledVersionTitle = ({ children }: { children: ReactNode }) => {
@@ -9,9 +17,17 @@ const StyledVersionTitle = ({ children }: { children: ReactNode }) => {
   );
 };
 
-const StyledListItemText = ({ children, tab = 0 }: { children: ReactNode; tab?: number }) => {
+const StyledListItemText = ({
+  children,
+  tab = 0,
+}: {
+  children: ReactNode;
+  tab?: number;
+}) => {
   return (
-    <ListItem sx={(theme: Theme) => ({ m: 0, p: 0, pl: theme.spacing(tab * 2) })}>
+    <ListItem
+      sx={(theme: Theme) => ({ m: 0, p: 0, pl: theme.spacing(tab * 2) })}
+    >
       -
       <ListItemText primary={children} />
     </ListItem>
@@ -25,6 +41,11 @@ export const ReleasePagePresenter = () => {
         リリース情報
       </Typography>
       <Divider sx={{ mb: 2 }} />
+
+      <StyledVersionTitle>[2.3.0] - 2024-09-04</StyledVersionTitle>
+      <List>
+        <StyledListItemText>タイム記録一覧にグラフを追加</StyledListItemText>
+      </List>
 
       <StyledVersionTitle>[2.2.0] - 2024-06-23</StyledVersionTitle>
       <List>
@@ -42,7 +63,9 @@ export const ReleasePagePresenter = () => {
 
       <StyledVersionTitle>[2.1.1] - 2024-06-20</StyledVersionTitle>
       <List>
-        <StyledListItemText>回転記号表現の矢印のスタイルを見やすく変更</StyledListItemText>
+        <StyledListItemText>
+          回転記号表現の矢印のスタイルを見やすく変更
+        </StyledListItemText>
         <StyledListItemText>
           タイマーストップをspaceキー以外でも可能に変更(一部対応してないキーあり)
         </StyledListItemText>
@@ -53,7 +76,9 @@ export const ReleasePagePresenter = () => {
 
       <StyledVersionTitle>[2.1.0] - 2024-06-17</StyledVersionTitle>
       <List>
-        <StyledListItemText>AO5,AO12を正しく計算するように修正</StyledListItemText>
+        <StyledListItemText>
+          AO5,AO12を正しく計算するように修正
+        </StyledListItemText>
         <StyledListItemText>
           スクランブルに表示する 2 回転の出現を少なくするように調整
         </StyledListItemText>
@@ -71,7 +96,9 @@ export const ReleasePagePresenter = () => {
 
       <StyledVersionTitle>[1.3.0] - 2024-05-31</StyledVersionTitle>
       <List>
-        <StyledListItemText>直近12回のタイムとスクランブルを表示する機能を追加</StyledListItemText>
+        <StyledListItemText>
+          直近12回のタイムとスクランブルを表示する機能を追加
+        </StyledListItemText>
         <StyledListItemText>
           新記録が出た時にタイムの上にNew Recordと表示する機能を追加
         </StyledListItemText>
@@ -79,13 +106,17 @@ export const ReleasePagePresenter = () => {
 
       <StyledVersionTitle>[1.2.0] - 2024-05-30</StyledVersionTitle>
       <List>
-        <StyledListItemText>5回平均(AO5)と12回平均(AO12)時間の表示機能の追加</StyledListItemText>
+        <StyledListItemText>
+          5回平均(AO5)と12回平均(AO12)時間の表示機能の追加
+        </StyledListItemText>
         <StyledListItemText>Cookieに直近12回の記録を保存</StyledListItemText>
       </List>
 
       <StyledVersionTitle>[1.1.3] - 2024-05-28</StyledVersionTitle>
       <List>
-        <StyledListItemText>スクランブル手順のモデル画像のレスポンシブ対応</StyledListItemText>
+        <StyledListItemText>
+          スクランブル手順のモデル画像のレスポンシブ対応
+        </StyledListItemText>
       </List>
 
       <StyledVersionTitle>[1.1.2] - 2024-05-24</StyledVersionTitle>
@@ -93,13 +124,17 @@ export const ReleasePagePresenter = () => {
         <StyledListItemText>
           スクランブル手順を一部修正(直行する回転記号のみが連続するように変更)
         </StyledListItemText>
-        <StyledListItemText>スクランブルの表示イメージを修正</StyledListItemText>
+        <StyledListItemText>
+          スクランブルの表示イメージを修正
+        </StyledListItemText>
       </List>
 
       <StyledVersionTitle>[1.1.1] - 2024-05-20</StyledVersionTitle>
       <List>
         <StyledListItemText>手順ページの一部追加</StyledListItemText>
-        <StyledListItemText>モデルの下に対応する回転記号を表示</StyledListItemText>
+        <StyledListItemText>
+          モデルの下に対応する回転記号を表示
+        </StyledListItemText>
       </List>
 
       <StyledVersionTitle>[1.1.0] - 2024-05-18</StyledVersionTitle>
@@ -116,8 +151,12 @@ export const ReleasePagePresenter = () => {
         <StyledListItemText>
           タイマー機能を、スペース長押し後に開始するように変更
         </StyledListItemText>
-        <StyledListItemText>ページのルーティングを正しく行なうように修正</StyledListItemText>
-        <StyledListItemText>回転矢印が一部変になっていたのを修正</StyledListItemText>
+        <StyledListItemText>
+          ページのルーティングを正しく行なうように修正
+        </StyledListItemText>
+        <StyledListItemText>
+          回転矢印が一部変になっていたのを修正
+        </StyledListItemText>
       </List>
 
       <StyledVersionTitle>[1.0.0] - 2024-05-15</StyledVersionTitle>
